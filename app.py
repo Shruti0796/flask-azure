7f6cfc794bed68cea09cb2acdcf288dee21b2e3b
shruti shanklesha <shruti07092006@gmail.com>
2026-02-26 11:42:20 +0530

    changed app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Flask app running on Azure 🚀"
+    return "hello it's shruti shanklesha"
 
 if __name__ == "__main__":
     app.run()
